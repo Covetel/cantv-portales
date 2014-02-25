191a9a0719199a65aa72a31dd2cfa9e7d3851887
Carlos Paredes <cparedes@covetel.com.ve>
2014-02-25 14:31:15 -0430

    Añadiendo javascript que controla el submenu en la plantilla de CANTV para Plone
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

---
+++ plone/cantv-theme/js/cantv.js
@@ -0,0 +1,16 @@
+site = $(location).attr('pathname');
+submenu_hogares = ['Telefonia', 'Internet', 'Televison', 'Promociones', 'Compras', 'Pagos y Fact'];
+submenu_emp_priv = ['Voz', 'Movil', 'Internet', 'TV', 'Datos', 'TI'];
+
+$(document).ready(function(){
+    if (site.match(/Plone$/gi)) {
+        for (i in submenu_hogares) {
+            $( "#submenu-left" ).append( '<li><a class="btn" href="http://localhost:8081/Plone/'+submenu_hogares[i]+'">'+submenu_hogares[i]+'</a></li>' );
+        }
+    }
+    if (site.match(/empresas-privadas/gi)) {
+        for (i in submenu_hogares) {
+           $( "#submenu-left" ).append( '<li><a class="btn" href="http://localhost:8081/Plone/empresas-privadas/'+submenu_emp_priv[i]+'">'+submenu_emp_priv[i]+'</a></li>' );
+        }
+    }
+});
