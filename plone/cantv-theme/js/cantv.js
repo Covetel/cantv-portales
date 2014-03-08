544640b0250143b4053673dec948ac7af1c079a3
Carlos Paredes <cparedes@covetel.com.ve>
2014-03-08 15:18:09 -0430

    Corrigiendo botones del submenu en la plantilla de CANTV para Plone
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- plone/cantv-theme/js/cantv.js
+++ plone/cantv-theme/js/cantv.js
@@ -1,16 +1,17 @@
 site = $(location).attr('pathname');
+host = $(location).attr('host');
 submenu_hogares = ['Telefonia', 'Internet', 'Televison', 'Promociones', 'Compras', 'Pagos y Fact'];
 submenu_emp_priv = ['Voz', 'Movil', 'Internet', 'TV', 'Datos', 'TI'];
 
 $(document).ready(function(){
     if (site.match(/Plone$/gi)) {
         for (i in submenu_hogares) {
-            $( "#submenu-left" ).append( '<li><a class="btn" href="http://localhost:8081/Plone/'+submenu_hogares[i].toLowerCase()+'">'+submenu_hogares[i]+'</a></li>' );
+            $( "#submenu-left" ).append( '<li><a class="btn" href="http://'+host+'/Plone/'+submenu_hogares[i].toLowerCase()+'">'+submenu_hogares[i]+'</a></li>' );
         }
     }
     if (site.match(/empresas-privadas/gi)) {
         for (i in submenu_hogares) {
-           $( "#submenu-left" ).append( '<li><a class="btn" href="http://localhost:8081/Plone/empresas-privadas/'+submenu_emp_priv[i].toLowerCase()+'">'+submenu_emp_priv[i]+'</a></li>' );
+           $( "#submenu-left" ).append( '<li><a class="btn" href="http://'+host+'/Plone/empresas-privadas/'+submenu_emp_priv[i].toLowerCase()+'">'+submenu_emp_priv[i]+'</a></li>' );
         }
     }
 });
